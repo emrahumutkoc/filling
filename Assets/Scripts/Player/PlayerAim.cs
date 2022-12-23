39d92ea7ea6cbf4039fc758534c3e44e5b865364
Emrah Umut Koç <emrahumutkoc@Emrahs-MacBook-Pro.local>
2022-12-23 19:13:45 +0300

    implementation of player aim and utils

---
+++ Assets/Scripts/Player/PlayerAim.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MRH.Utils;
+
+public class PlayerAim : MonoBehaviour
+{
+    private Transform aimTransform;
+
+    private void Awake()
+    {
+        aimTransform = transform.Find("Aim");
+    }
+
+    private void Update()
+    {
+        Vector3 mousePosition = Utils.GetMouseWorldPosition();
+
+        Vector3 aimDirection = (mousePosition - transform.position).normalized;
+        float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
+        aimTransform.eulerAngles = new Vector3(0, 0, angle);
+        Debug.Log("angle" + angle);
+    }
+}
